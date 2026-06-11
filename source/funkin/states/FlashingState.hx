a788139acd461be77416986a51909990076596b0
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-11 12:54:59 -0300

    Adding Touch Support to FlashingState

--- source/funkin/states/FlashingState.hx
+++ source/funkin/states/FlashingState.hx
@@ -9,16 +9,20 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 
+#if mobile
+import flixel.input.touch.FlxTouch;
+#end
+
 class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
-	
+
 	var warnText:FlxText;
-	
+
 	override function create()
 	{
 		super.create();
-		
+
 		warnText = new FlxText(0, 0, FlxG.width, "
 WARNING!\n
 This mod contains effects that may trigger photosensitivity.\n
@@ -30,37 +34,54 @@ You may change this anytime in the Options menu.
 		warnText.screenCenter();
 		add(warnText);
 	}
-	
+
 	override function update(elapsed:Float)
 	{
-		if (!leftState && (controls.ACCEPT || controls.BACK)) {
+		var accept:Bool = controls.ACCEPT;
+		var back:Bool = controls.BACK;
+
+		#if mobile
+		for (touch in FlxG.touches.justStarted())
+		{
+			if (touch.x < FlxG.width * 0.5)
+				back = true;
+			else
+				accept = true;
+		}
+		#end
+
+		if (!leftState && (accept || back))
+		{
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
-			
-			ClientPrefs.photosensitive = controls.BACK;
+
+			ClientPrefs.photosensitive = back;
 			FlxG.sound.play(Paths.sound('confirmMenu'));
-			
-			if (controls.BACK)
+
+			if (back)
 			{
 				FlxTween.tween(warnText, {alpha: 0}, 1,
 				{
-					onComplete: function(twn:FlxTween) {
+					onComplete: function(twn:FlxTween)
+					{
 						FlxG.switchState(TitleState.new);
 					}
 				});
 			}
 			else
 			{
-				FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-					new FlxTimer().start(0.5, function(tmr:FlxTimer) {
+				FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker)
+				{
+					new FlxTimer().start(0.5, function(tmr:FlxTimer)
+					{
 						FlxG.switchState(TitleState.new);
 					});
 				});
 			}
-			
+
 			leftState = true;
 		}
-		
+
 		super.update(elapsed);
 	}
 }
