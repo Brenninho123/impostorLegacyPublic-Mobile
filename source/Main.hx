f43aebcb2c2041b3449d401aa869b360853bda9f
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-11 06:22:32 -0300

    Quente

--- source/Main.hx
+++ source/Main.hx
@@ -10,6 +10,10 @@ import flixel.input.keyboard.FlxKey;
 
 import funkin.backend.DebugDisplay;
 
+#if android
+import mobile.Storage;
+#end
+
 @:nullSafety(Strict)
 class Main extends Sprite
 {
@@ -17,7 +21,7 @@ class Main extends Sprite
 	public static final NMV_VERSION:String = '1.0';
 	public static final FUNKIN_VERSION:String = '0.2.7';
 	public static final LEGACY_VERSION:String = '1.1.0';
-	
+
 	public static final startMeta =
 		{
 			width: 1280,
@@ -27,80 +31,75 @@ class Main extends Sprite
 			startFullScreen: false,
 			initialState: funkin.states.TitleState
 		};
-		
+
 	static function __init__()
 	{
 		funkin.utils.MacroUtil.haxeVersionEnforcement();
-		
 		openfl.utils._internal.Log.level = openfl.utils._internal.Log.LogLevel.INFO;
 	}
-	
+
 	public function new()
 	{
 		super();
-		
+
+		#if android
+		Storage.init();
+		#end
+
 		funkin.Mods.updateModList();
 		funkin.Mods.loadTopMod();
-		
+
 		#if (CRASH_HANDLER && !debug)
 		funkin.backend.CrashHandler.init();
 		#end
-		
+
 		initHaxeUI();
-		
+
 		#if (windows && cpp)
-		// cpp.Windows.setDarkMode();
 		cpp.Windows.setDpiAware();
 		#end
-		
-		// load save data before creating FlxGame
+
 		ClientPrefs.loadDefaultKeys();
 		ClientPrefs.tryBindingSave('funkin');
-		
+
 		final game = new funkin.backend.FunkinGame(startMeta.width, startMeta.height, Init, startMeta.fps, startMeta.fps, true, startMeta.startFullScreen);
-		
-		// btw game has to be a variable for this to work ig - Orbyy
+
 		@:privateAccess
 		game._customSoundTray = funkin.objects.FunkinSoundTray;
 		addChild(game);
-		
-		// prevent accept button when alt+enter is pressed
+
 		FlxG.stage.addEventListener(openfl.events.KeyboardEvent.KEY_DOWN, (e) -> {
 			if (e.keyCode == FlxKey.ENTER && e.altKey) e.stopImmediatePropagation();
 		}, false, 100);
-		
+
 		DebugDisplay.init();
-		
+
 		FlxG.signals.gameResized.add(onResize);
-		
+
 		#if DISABLE_TRACES
 		haxe.Log.trace = (v:Dynamic, ?infos:haxe.PosInfos) -> {}
 		#end
-		
+
 		#if sys
 		FlxG.stage.window.onClose.add(function() {
 			@:privateAccess MusicBeatState.addPlayTimeDelta();
 			ClientPrefs.flush();
-			Sys.println('saved data');
 			funkin.Mods.writeModList();
-			Sys.println('saved mods');
-			
+
 			#if hxvlc
-			hxvlc.util.Handle.dispose(); // this is jsut from base game ok
+			hxvlc.util.Handle.dispose();
 			#end
-			
-			Sys.println('GOOD BYE CRUEL WORLD');
 
 			Sys.exit(0);
 		});
 		#end
 	}
-	
+
 	@:access(flixel.FlxCamera)
 	static function onResize(w:Int, h:Int)
 	{
 		final scale:Float = Math.max(1, Math.min(w / FlxG.width, h / FlxG.height));
-		
+
 		if (FlxG.cameras != null)
 		{
 			for (i in FlxG.cameras.list)
@@ -108,13 +107,11 @@ class Main extends Sprite
 				if (i != null && i.filters != null) resetSpriteCache(i.flashSprite);
 			}
 		}
-		
+
 		if (FlxG.game != null)
-		{
 			resetSpriteCache(FlxG.game);
-		}
 	}
-	
+
 	@:nullSafety(Off)
 	public static function resetSpriteCache(sprite:Sprite):Void
 	{
@@ -125,7 +122,7 @@ class Main extends Sprite
 			sprite.__cacheBitmapData = null;
 		}
 	}
-	
+
 	function initHaxeUI():Void
 	{
 		#if haxeui_core
